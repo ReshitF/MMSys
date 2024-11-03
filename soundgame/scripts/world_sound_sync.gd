662d6333eb53e13ba15200bd68a1394ae38a1d97
ThanosSkentzos <kalligerist@gmail.com>
2024-11-03 17:07:21 +0100

    without attenuation direction does not work

--- soundgame/scripts/world_sound_sync.gd
+++ soundgame/scripts/world_sound_sync.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @onready var sound_source_2d = $"Enemy"  # Reference to your 2D sound source
-@onready var audio_player_3d = $"sound_node/step 1"  # Reference to the 3D audio player
-@onready var listener_3d = $"sound_node/microphone"  # Reference to the 3D listener (optional)
+@onready var audio_player_3d = $"sound_node/step_1"  # Reference to the 3D audio player
+@onready var listener_3d = $"sound_node/camera"  # Reference to the 3D listener (optional)
 @onready var player_2d = $Player
 # Fixed z-position for 3D audio, since we're only working with x and y from 2D
 const FIXED_Z_POSITION = 0.0  
+	
 
 func _process(delta: float) -> void:
 	# Update the 3D audio position based on the 2D position of the sound source
@@ -17,3 +18,5 @@ func _process(delta: float) -> void:
 	var player_pos_2d = player_2d.global_position
 	listener_3d.position = Vector3(player_pos_2d.x, player_pos_2d.y, FIXED_Z_POSITION)
 	print("Listener3D Position:", listener_3d.position)
+	print("Distance ", listener_3d.position.distance_to(audio_player_3d.position))
+	
