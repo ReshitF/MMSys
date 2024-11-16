f6b586d812a7b60cd4b4f9eb968e5b1dff74f6c9
ThanosSkentzos <kalligerist@gmail.com>
2024-11-16 15:03:36 +0100

    enemy path

--- soundgame/scripts/EnemyPath.gd
+++ soundgame/scripts/EnemyPath.gd
@@ -1,8 +1,7 @@
-extends CharacterBody2D
-#@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
+extends PathFollow2D
 
 @export var speed = 100
 func _process(delta: float) -> void:
-	#path_follow.progress += speed*delta
+	self.progress += speed*delta
 	#print(path_follow.progress)
-	pass
+	#pass
