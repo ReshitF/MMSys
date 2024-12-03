38eeced37bedade2f4c104313c118b2cfeb79f47
ThanosSkentzos <kalligerist@gmail.com>
2024-12-03 23:27:48 +0100

    chase

--- soundgame/scripts/EnemyAudioRange.gd
+++ soundgame/scripts/EnemyAudioRange.gd
@@ -1,6 +1,6 @@
 extends Area2D
-
-
+@onready var navigation_agent:NavigationAgent2D = $"../NavigationAgent2D"
+@onready var enemy:CharacterBody2D = $".."
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,4 +14,8 @@ func _process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "NoiseRange" and (area.collision_layer & (1 << 0)):
 		print("ENEMY AudioRange has detected PLAYER NoiseRange")
+		#navigation_agent.target_position = area.global_position
+		enemy.chase=true
+		
+		
 	pass # Replace with function body.
--- soundgame/scripts/EnemySoundSync.gd
+++ soundgame/scripts/EnemySoundSync.gd
@@ -2,11 +2,26 @@ extends CharacterBody2D
 
 @onready var audio_player_3d = $"NoiseRange/step_1"  # Reference to the 3D audio player
 @onready var audio_player_3d_2 = $"NoiseRange/step_2"  # Reference to the 3D audio player
+@export var target_to_chase: CharacterBody2D = null
+@onready var navigation_agent :NavigationAgent2D =$NavigationAgent2D
+@onready var chase :bool = false
 
 const FIXED_Z_POSITION = 0.0  
-
+const SPEED = 100
 func _process(delta: float) -> void:
 	var pos_2d = self.global_position
 	audio_player_3d.position = Vector3(pos_2d.x, pos_2d.y, FIXED_Z_POSITION)
 	audio_player_3d_2.position = Vector3(pos_2d.x, pos_2d.y, FIXED_Z_POSITION)
 	#print("AudioPlayer3D Position:", audio_player_3d.position)
+	if target_to_chase != null:
+		navigation_agent.target_position = target_to_chase.global_position			
+	#if navigation_agent.tdddarget_position != null:
+	if self.chase:
+		velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * SPEED
+		var target = navigation_agent.target_position
+		var dist = global_position.distance_to(target)
+		if dist>500:
+			self.chase=false
+			velocity= velocity*0
+	if !navigation_agent.is_target_reached():
+		move_and_slide()
