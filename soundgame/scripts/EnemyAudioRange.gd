04e0b78098f7307da3376001b9e30b8a29e9c932
ThanosSkentzos <kalligerist@gmail.com>
2024-12-05 19:17:01 +0100

    no move, no chase

--- soundgame/scripts/EnemyAudioRange.gd
+++ soundgame/scripts/EnemyAudioRange.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @onready var navigation_agent:NavigationAgent2D = $"../NavigationAgent2D"
 @onready var enemy:CharacterBody2D = $".."
+@onready var player:CharacterBody2D = $"../../Player"
+#@onready var 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,10 +16,20 @@ func _process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "NoiseRange" and (area.collision_layer & (1 << 0)):
 		print("ENEMY AudioRange has detected PLAYER NoiseRange")
-		#navigation_agent.target_position = area.global_position
-		enemy.chase=true
-		print("CHASING")
-
-		
-		
-	pass # Replace with function body.
+		var player_velocity_meter = player.velocity.length()
+		if player_velocity_meter>0:
+			print("player moving --> CHASING MODE")
+			enemy.chase=true
+		else:
+			print("player not moving --> not chasing")
+func _on_area_exited(area: Area2D) -> void:
+	if area.name == "NoiseRange" and (area.collision_layer & (1 << 0)):
+		var player_velocity_meter = player.velocity.length()
+		if player_velocity_meter>0:
+			print("player moving --> CHASING MODE")
+			enemy.chase=true
+		else:
+			print("player not moving --> not chasing")
+#func _on_area_exited(area:Area2D) -> void:
+	#if area.name == "NoiseRange" and (area.collision_layer & (1 << 0)):
+		#print("player exits")
